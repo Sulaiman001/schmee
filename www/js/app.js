48b8b99d28ce98485a5f389a8e29d28afe7297f8
Glen Baker <iepathos@gmail.com>
2015-09-10 19:34:22 -0400

    Added contacts retrieval and tab

---
+++ www/js/app.js
@@ -0,0 +1,76 @@
+// Schmee Notification Improvements by Mocha Dick
+var debug = true;
+
+angular.module('schmee', ['ionic', 'ngCordova'])
+
+.controller('ContactsCtrl', function($scope, $cordovaContacts) {
+  var isAndroid = ionic.Platform.isAndroid();
+  
+  $scope.updateContacts = function() {
+    
+  } 
+
+  $scope.loadContacts = function() {
+    if (isAndroid) {
+      $cordovaContacts.find({multiple: true}).then(function(res) {
+        $scope.contacts  = res;
+      });
+    } else {
+      $scope.contacts = [
+        { displayName: 'Glen Baker'
+        },
+        { displayName: 'Robert Hawk'
+        },
+        { displayName: 'Mocha Dick'
+        }
+      ];
+    }
+  }
+
+})
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  $ionicConfigProvider.tabs.position('bottom');
+
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "templates/home.html"
+        }
+      }
+    })
+    .state('tabs.contacts', {
+      url: "/contacts",
+      views: {
+        'contacts-tab': {
+          templateUrl: "templates/contacts.html",
+          controller: "ContactsCtrl"
+        }
+      }
+    });
+
+
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
